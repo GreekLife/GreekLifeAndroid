d27cb228e7261a842ca3375f28c8dce9f5f2d633
Jonah Elbaz <jonah-elbaz@hotmail.com>
2017-12-18 16:32:23 -0500

    Setting up home page + login views for base

--- GreekLife/app/src/main/java/fraternityandroid/greeklife/MainActivity.java
+++ GreekLife/app/src/main/java/fraternityandroid/greeklife/MainActivity.java
@@ -1,7 +1,10 @@
 package fraternityandroid.greeklife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    EditText mEmail = (EditText) findViewById(R.id.Email);
+    EditText mPassword = (EditText) findViewById(R.id.Password);
+
+    public void Login(View view) {
+
+
+        
+        Intent goToHomePage = new Intent(this, HomePage.class);
+        startActivity(goToHomePage);
     }
 }

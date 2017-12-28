e19a263c527a9ba291020e44e4856a1776a6cfc9
Jonah Elbaz <jonah-elbaz@hotmail.com>
2017-12-28 10:38:32 -0500

    setup poll activity

---
+++ GreekLife/app/src/main/java/fraternityandroid/greeklife/PollActivity.java
@@ -0,0 +1,19 @@
+package fraternityandroid.greeklife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PollActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poll);
+
+        getPolls();
+    }
+
+    public void getPolls() {
+
+    }
+}
